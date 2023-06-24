180d2f3d6b35bf1461323456e0c2cc7b3b53971d
PHAMHAIVIET <125327201+PHAMHAIVIET@users.noreply.github.com>
2023-06-24 13:02:46 +0700

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,11 @@
 <?php
 
 $sname= "localhost";
-$unmae= "root";
-$password = "";
+$unmae= "
+id20940878_haiviet128";
+$password = "1282003aA!";
 
-$db_name = "php_db";
+$db_name = "id20940878_dblogin";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
